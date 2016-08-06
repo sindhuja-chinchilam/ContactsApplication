8ccfb352dd2adb2c026fcf0a61ab60a8351fce70
avinash <chinchilamsindhuja@gmail.com>
2016-08-06 17:12:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/xinthe/contactsapplication/EditContacts.java
@@ -0,0 +1,14 @@
+package com.example.xinthe.contactsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class EditContacts extends AppCompatActivity {
+EditText 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contacts);
+    }
+}
